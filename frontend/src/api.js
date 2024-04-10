732dfe8c0cad17b49d3dbf7e1233b180b0123d9c
U-LDBM\david <david_bm42@hotmail.com>
2024-04-10 00:52:09 -0400

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+const apiUrl = "/choreo-apis/djangoreactremittancecalc/backend/rest-api-be2/v1.0";
 
 const api = axios.create({
     // this imports anything that is inside an emvironment variable
